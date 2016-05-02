8280ab794bbaae6caaff971ca96850816280b8c6
Nishant Nawarkhede <nishant.nawarkhede@gmail.com>
2016-05-02 19:48:14 +0530

    added weather

--- info.py
+++ info.py
@@ -1,5 +1,16 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
+print 'Weather ...'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Mumbai,in&appid=51802c40fcf278769218f282d61d324e&units=metric'
+data = requests.get(url)
+data = json.loads(data.content)
+print '%s -> Min %s - Max %s | humidity %s ' % (data['name'],
+                                                data['main']['temp_min'],
+                                                data['main']['temp_max'],
+                                                data['main']['humidity'])
+
 
 print 'Headlines ...'
 news = []
